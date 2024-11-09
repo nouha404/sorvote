0fff824eedacc81502be1eab6998ba5a87a7fa4e
nouha404 <aboubacar.marega@ism.edu.sn>
2024-11-09 19:35:57 +0100

    Project structure

---
+++ src/main/java/com/iut/sorbonne/SorbonneApplication.java
@@ -0,0 +1,13 @@
+package com.iut.sorbonne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorbonneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorbonneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iut/sorbonne/SorbonneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iut.sorbonne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorbonneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
